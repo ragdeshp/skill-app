1c9c3e7ba01d2d52e033984a2b16257151023dcd
Raghavendra Deshpande <ragdeshp@in.ibm.com>
2019-10-23 14:22:41 +0530

    Added Cloudant

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-var cloudant = require('cloudant');
+//var cloudant = require('cloudant');
 var express = require('express');
 var bodyParser = require('body-parser');
 var stringSimilarity = require('string-similarity');
 let secureEnv = require('secure-env');
-global.env = secureEnv({secret:'mySecretPassword'});
+//global.env = secureEnv({secret:'mySecretPassword'});
 
 var app = express();
 
@@ -15,7 +15,13 @@ app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
   res.sendFile("views/index.html", {"root": __dirname});
 });
-var cloudant = cloudant({account:global.env.cloudantusername, password:global.env.cloudantpassword});
+
+var Cloudant = require('@cloudant/cloudant');
+var cloudant = Cloudant({url: "https://66fe83f7-f925-45ed-b758-5d0a75f7cbee-bluemix:0d5dffb251e4115ea1e9da9476c37df07e4fb2e6b79e222a960dd4fad0675ac7@66fe83f7-f925-45ed-b758-5d0a75f7cbee-bluemix.cloudantnosqldb.appdomain.cloud"});
+var dbname = 'mydb';
+
+
+//var cloudant = cloudant({account:global.env.cloudantusername, password:global.env.cloudantpassword});
 
 var responsequiz1;
 var responsequiz2;
@@ -24,7 +30,7 @@ var time;
 var url;
 var counter=0;
 
-quizdb = cloudant.db.use(global.env.dbname);
+quizdb = cloudant.db.use(dbname);
 
 
 app.post('/quizsubmission', function(req,res){
